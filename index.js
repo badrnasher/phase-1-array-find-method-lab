48a38f5478554854a2e9964b98d5bb17543fd501
Badr Nasher <badrnasher@gmail.com>
2023-03-30 01:46:00 +0300

    Assignment Done

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // code your solution here
+const record = [
+  { year: "2015", result: "W" },
+  { year: "2014", result: "N/A" },
+  { year: "2013", result: "L" },
+  //...
+];
+
+function superbowlWin(arr) {
+  for (const record of arr) {
+    if (record.result === "W") {
+      return record.year;
+    }
+  }
+}
